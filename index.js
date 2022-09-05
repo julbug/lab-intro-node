5587c406a700493813ec76d33ec4556b4f197119
Julia Dannerth <jdannerth@gmail.com>
2022-09-05 15:33:11 -0400

    done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
+const { test } = require("mocha");
+
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  min() {}
 
-  sum() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  avg() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
+
+  sum() {
+    return this.items.reduce((total, value) => {
+      return total + value;
+    }, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
